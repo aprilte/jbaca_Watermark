5d3af40bbfc3f9af07950c8dd42968df5894fc4c
aprilte <apriltexs@gmail.com>
2022-11-12 10:28:10 +0900

    1112::2

--- Watermark/task/src/watermark/Main.kt
+++ Watermark/task/src/watermark/Main.kt
@@ -1,5 +1,38 @@
 package watermark
 
+import java.awt.image.BufferedImage
+import java.io.File
+import java.lang.Exception
+import javax.imageio.ImageIO
+
 fun main() {
-    println("Hello World!")
+    inspectImg()
+}
+
+fun inspectImg() {
+    println("Input the image filename:")
+    val myPath = System.getProperty("user.dir")
+    val fileName = readln()
+    val imageFile = File(myPath + "\\" + fileName)
+
+    try {
+        val image: BufferedImage = ImageIO.read(imageFile)
+        println("Image file: $fileName")
+        println("Width: ${image.width}")
+        println("Height: ${image.height}")
+        println("Number of components: ${image.colorModel.numComponents}")
+        println("Number of color components: ${image.colorModel.numColorComponents}")
+        println("Bits per pixel: ${image.colorModel.pixelSize}")
+        print("Transparency: ")
+        println(
+            when(image.transparency) {
+                1, -> "OPAQUE"
+                2, -> "BITMASK"
+                3, -> "TRANSLUCENT"
+                else -> "NONE"
+            }
+        )
+    } catch (ex: Exception) {
+        println("The file ${fileName} doesn't exist.")
+    }
 }
