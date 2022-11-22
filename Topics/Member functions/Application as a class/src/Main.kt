d00ff35b874087a5a12076d1ceefea588decd2f6
aprilte <apriltexs@gmail.com>
2022-11-23 01:22:30 +0900

    1123::1

---
+++ Topics/Member functions/Application as a class/src/Main.kt
@@ -0,0 +1,9 @@
+class Application(val name: String) {
+    fun run(a: String, b: String, c: String ) {
+        println(this.name)
+        println(a)
+        println(b)
+        println(c)
+    }
+    // write the run method here
+}
