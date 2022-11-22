205e0f79ee1843edbeb0b28b9fd23bcc13b480b3
aprilte <apriltexs@gmail.com>
2022-11-23 01:29:41 +0900

    1123::2

--- Topics/Member functions/Volume of a cube/src/Main.kt
+++ Topics/Member functions/Volume of a cube/src/Main.kt
@@ -1,6 +1,4 @@
 class Box(var height: Double, var width: Double, var length: Double) {
-    fun getVolume(): Double {
-        return this.height * this.width * this.length
-    }
+    fun getVolume() = this.height * this.width * this.length
     // write the getVolume method here
 }
