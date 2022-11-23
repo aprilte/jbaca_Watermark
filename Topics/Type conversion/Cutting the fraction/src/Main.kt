886189b434fa31468a9a9ced8908490e7f09b89e
aprilte <apriltexs@gmail.com>
2022-11-24 01:09:08 +0900

    1124::1

---
+++ Topics/Type conversion/Cutting the fraction/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    // write your code here
+    val input: String = readln()
+    val innum: Double = input.toDouble()
+    val inint: Long = innum.toLong()
+    println(inint)
+}
---
+++ Topics/Type conversion/The superior forces of the Empire/src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    // write your code here
+    val emp = readln().toInt()
+    val rebel = readln().toInt()
+    println(emp/rebel)
+}
